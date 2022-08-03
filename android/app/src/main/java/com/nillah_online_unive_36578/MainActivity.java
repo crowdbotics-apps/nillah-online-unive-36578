0b0aa77ecc0d9a7d840e012ec3dadc9869e48221
Crowdbotics <team@crowdbotics.com>
2022-08-03 06:41:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/nillah_online_unive_36578/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nillah_online_unive_36578;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "nillah_online_unive_36578";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "nillah_online_unive_36578",
+  url: "https://nillah_online_unive_36578.botics.co",
+  api: "https://nillah_online_unive_36578.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
